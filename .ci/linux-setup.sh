d82bddc7e71e6f4004993f3ea6745c7b53cc40a1
David Wilder <dwilder@us.ibm.com>
2019-11-06 11:57:46 -0800

    travis: Add multi-arch support for ci.
    -Add arch list to travis, including amd64, arm64 and ppc64le
    -Eliminate cross build for arm64, switching to native build.
    -Only setup hugepages if running a test
    -Move alternate package install to .ci/linux-setup.sh
    -Cleanup matrix, exclude arm64 and ppc64le from running tests.
    -Only build Doc on amd64.
    
    A build report example can be found here [1]
        [0] http://travis-ci.org/
        [1] https://travis-ci.org/djlwilder/dpdk/builds/xxxxx
    
    Signed-off-by: David Wilder <dwilder@us.ibm.com>

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,7 +3,26 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-# setup hugepages
-cat /proc/meminfo
-sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
-cat /proc/meminfo
+if [ "$EXTRA_PACKAGES" = "1" ]; then
+        # Add extra packages
+        sudo apt-get install -y \
+             libbsd-dev \
+             libpcap-dev \
+             libcrypto++-dev \
+             libjansson4
+fi
+
+if [ "$BUILD_DOCS" = "1" ]; then
+        # Add extra packages
+        sudo apt-get install -y \
+             doxygen \
+             graphviz \
+             python3-sphinx
+fi
+
+if [ "$RUN_TESTS" = "1" ]; then
+        # setup hugepages
+        cat /proc/meminfo
+        sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
+        cat /proc/meminfo
+fi
