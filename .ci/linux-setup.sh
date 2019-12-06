a3e17ad9dcbd9c90145cd0a7f3bac56c8edc08db
David Wilder <dwilder@us.ibm.com>
2019-12-06 11:23:53 -0800

    travis: Add multi-arch support for ci.

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,7 +3,29 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-# setup hugepages
-cat /proc/meminfo
-sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
-cat /proc/meminfo
+if [ "$EXTRA_PACKAGES" = "1" ]; then
+        sudo apt-get install -y \
+             libbsd-dev \
+             libpcap-dev \
+             libcrypto++-dev \
+             libjansson4
+fi
+
+if [ "$BUILD_DOCS" = "1" ]; then
+        sudo apt-get install -y \
+             doxygen \
+             graphviz \
+             python3-sphinx
+fi
+
+if [ "$BUILD_32BIT" = "1" ]; then
+       sudo apt-get install -y \
+            gcc-multilib
+fi
+
+if [ "$RUN_TESTS" = "1" ]; then
+        # setup hugepages
+        cat /proc/meminfo
+        sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
+        cat /proc/meminfo
+fi
