c64df97cfe5bcc4c9810a1a24dcdc0193ea2eb2d
David Wilder <dwilder@us.ibm.com>
2020-02-14 15:22:05 -0800

    devtools: allow test-null.sh to run on ppc64le
    Memory requirements are larger for ppc64le
    due to a higher RTE_MAX_LCORE value.
    
    Signed-off-by: David Wilder <dwilder@us.ibm.com>
    Reviewed-by: David Christensen <drc@linux.vnet.ibm.com>

--- devtools/test-null.sh
+++ devtools/test-null.sh
@@ -27,6 +27,6 @@ else
 fi
 
 (sleep 1 && echo stop) |
-$testpmd -c $coremask --no-huge -m 20 \
+$testpmd -c $coremask --no-huge -m 30 \
 	$libs -w 0:0.0 --vdev net_null1 --vdev net_null2 $eal_options -- \
 	--no-mlockall --total-num-mbufs=2048 $testpmd_options -ia
